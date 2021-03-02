9651d18f716e3e3421d6d9dfb864f035aae620dc
Gabriel Haab <gabrielhaab@gmail.com>
2021-03-01 21:56:44 -0700

    Fixes on previous functions + network selection

--- 498.py
+++ 498.py
@@ -9,6 +9,7 @@ Version 1.0 February 2021, - Added serial communication
 Version 1.1 February 2021, - Tkinter + Serial Connection
 Version 1.2 February 2021, - Tkinter Images + about menu + some structures
 Version 1.3 February 2021, - Loading window + some structures
+Version 1.4 March 2021, - Fixes on previous functions + network selection
 '''
 
 # Imports
@@ -22,6 +23,7 @@ import threading
 import re
 import base64
 import multiprocessing
+import csv
 
 serial_port = "COM4"
 baud_rate = 115200
@@ -136,6 +138,42 @@ def send_command(command):
     #print("Received: " + rcvd)
     return rcvd
     
+def select_network(network_name): 
+    ESSID = network_name.get()
+    Privacy = data[ESSID][0]
+    BSSID = data[ESSID][1]
+    Channel = data[ESSID][2]
+    
+    print("You Selected: " + ESSID + " | " + Privacy + " | " + BSSID + " | " + Channel)
+    
+def read_csv():
+    try:
+        with open("search.cap-01.csv",'r') as f:
+            with open("search.cap-01.csv_updated.csv",'w') as f1:
+                next(f) # skip header line
+                for line in f:
+                    f1.write(line)
+        f1.close()
+        f.close()
+        data = {} 
+        
+        with open('search.cap-01.csv_updated.csv', "r") as csvfile:
+            csv_reader  = csv.DictReader(csvfile, delimiter=',')
+            for row in csv_reader:
+                #print(row)
+                if row[' ESSID'] != None:
+                    if row[' ESSID'].strip() != "":
+                        ESSID = row[' ESSID'].strip()
+                        Privacy = row[' Privacy'].strip()
+                        BSSID = row['BSSID'].strip()
+                        Channel = row[' channel'].strip()
+                        
+                        #print(ESSID + " | " + Privacy + " | " + BSSID + " | " + Channel)
+                        data[ESSID] = (Privacy,BSSID,Channel)
+        return data
+    except Exception as err:
+        print("Error: " + str(err))
+        return 0
 '''
 This function will send the command to the pi via serial
 '''
@@ -218,78 +256,57 @@ def start_terminal():
     #wait_Login()
     terminal_commands()
 
-def wardriving():
-    wardriving_window = tk.Toplevel()
-    wardriving_window.title("About") 
-    wardriving_window.resizable(False, False)      
-    label1 = tk.Label(wardriving_window, text="This is the window for WarDriving.")
-    
-    close_button=tk.Button(wardriving_window,text='CLOSE',command=wardriving_window.destroy, font=("Helvetica 16 bold"))
-    
-    label1.grid(row=0,column=0)
-    close_button.grid(row=1,column=0) 
-    
-    wardriving_window.mainloop()
-
-
-def transfer_over_serial_thread(file_name):
-    '''
-    p2 = multiprocessing.Process(target=transfer_over_serial, args=(ser,file_name, ))
-    p2.start()     
-    
-    '''
-    print("thread transfer")
-    y = threading.Thread(target=transfer_over_serial, args=(file_name,))
-    y.start()
-    y.join()
-    
 def transfer_over_serial(file_name):
-    b64data = send_command("base64 " + file_name + " -w 0 | wc -c && echo") 
-    print("Lenght to be transferred: " + str(b64data), flush=True)
-    #loading_bar(int(int(b64data)/12000),"Transferring file\n" + file_name)
-    b64data = send_command("base64 " + file_name + " -w 0 && echo") 
-    print("Received: " + str(len(b64data)))
-    
-    print("Creating the file...")
-    b64data = b64data.encode()
-    if '/' in file_name:
-        file_name = file_name.split('/')[-1]
-    with open(file_name, "wb") as fh:
-        fh.write(base64.decodebytes(b64data))    
-    print("Done transferring the file: " + file_name)    
+    try:
+        b64data = send_command("base64 " + file_name + " -w 0 | wc -c && echo") 
+        print("Lenght to be transferred: " + str(b64data), flush=True)
+        #loading_bar(int(int(b64data)/12000),"Transferring file\n" + file_name)
+        b64data = send_command("base64 " + file_name + " -w 0 && echo") 
+        print("Received: " + str(len(b64data)))
+        
+        print("Creating the file...")
+        b64data = b64data.encode()
+        if '/' in file_name:
+            file_name = file_name.split('/')[-1]
+        with open(file_name, "wb") as fh:
+            fh.write(base64.decodebytes(b64data))    
+        print("Done transferring the file: " + file_name)    
+        return 1
+    except Exception as err:
+        print("Error: " + str(err))
+        return 0
     
 def loading_bar(time_to_load, message):
-    print("process loading bar")
-    #p1 = multiprocessing.Process(target=loading_bar_1, args=(time_to_load, message, ))
-    #p1.start() 
-    x = threading.Thread(target=loading_bar_1, args=(time_to_load, message,))
-    x.start()
-    x.join()
+    x_thread = threading.Thread(target=loading_bar_1, args=(time_to_load, message, root,))
+    x_thread.run()
     
-def loading_bar_1(time_to_load, message): 
+def loading_bar_1(time_to_load, message, root): 
     try:
-        load_window = tk.Tk()
+        load_window = tk.Toplevel(root)
         load_window.resizable(False, False)
         load_window.title('Results')
-            
+        
         # Progress bar widget 
         label = tk.Label(load_window,text=message,font=("Helvetica 16 bold"))
-        
         progress = Progressbar(load_window, orient = tk.HORIZONTAL, length = 100, mode = 'determinate') 
         label.pack(pady = 20, padx = 20,)
         progress.pack(pady = 20, padx = 20,ipady = 15, ipadx = 50) 
         load_window.update()
-        
+        root.update()
         temp = time_to_load/10
         
         for i in range(1,11):
             if i != 10:
                 progress['value'] = i * 10
             load_window.update_idletasks() 
-            load_window.after(int(temp * 1000))
+            #load_window.after(int(temp * 1000))
+            time.sleep(temp)
+            root.update()
             
         progress['value'] = 100 
         load_window.destroy()
+        print("Done with loading bar")
+        
     except Exception as err:
         print("error: " + str(err))
         messagebox.showinfo("ERROR", message + " Cancelled")  
@@ -323,17 +340,17 @@ def rogueAP():
     rogueAP_window.mainloop()
 
 def wifi_Assessment():
+    
     Assessment_window = tk.Toplevel()
     Assessment_window.title("Assessment") 
     Assessment_window.resizable(False, False)      
     label1 = tk.Label(Assessment_window, text="This is the window for the Wireless Assessment.")
     
-    scan_button=tk.Button(Assessment_window,text='Scan for Networks',command=lambda: scanNetwork(Assessment_window), font=("Helvetica 16 bold"))
-    select_button=tk.Button(Assessment_window,text='Select Network',command=assessment_manager, font=("Helvetica 16 bold"))
-    
+    scan_button=tk.Button(Assessment_window,text='Scan for Networks',command=lambda: scanNetwork(Assessment_window, scan_button), font=("Helvetica 16 bold"))
+
     label1.grid(row=0,column=0)
     scan_button.grid(row=1,column=0) 
-    select_button.grid(row=5,column=0) 
+
     Assessment_window.mainloop()    
 
 def assessment_manager():
@@ -355,9 +372,7 @@ def analyze_traffic():
     # This function will perform a passive or active scan. (Choose one)
     print("Function to analyze the network")
     
-def encryption_Manager():
-    #encryption = open("search.cap")
-    
+def encryption_Manager(encryption):
     if encryption == "WEP":
         print ("The Encryption is WEP") 
         
@@ -381,21 +396,54 @@ def capture_handshake():
     print("capture_handshake")
 
 def remove_temp():
-    send_command("rm temp/*") 
+    print("Deleting the TEMP folder")
+    send_command1("rm /root/temp/*") 
+    time.sleep(2)
     
-def scanNetwork(Assessment_window):
+def scanNetwork(Assessment_window,scan_button):
+    global data
+    
     remove_temp()
-    monitor_mode("enable", "wlan1")
-    send_command1("timeout 15s airodump-ng mon0 -w /root/temp/search.cap &> file") 
-    done = loading_bar(35,'Scanning Networks')
-    #transfer over the .csv file
-    transfer_over_serial_thread('/root/temp/searc-01.kismet.csv')
     
-    w = OptionMenu(master, variable, *data.keys())
-    w.pack()
+    monitor_mode("enable", "wlan1")
+    
+    print("sending scanning command")
+    
+    send_command1("timeout 15s airodump-ng mon0 -w /root/temp/search.cap &> file") 
+    
+    loading_bar(20,'Scanning for Networks')
+    
+    #transfer over the .csv file
+    print("Transferring the file")
+    x = 1
+    while x == 0:
+        x = transfer_over_serial('/root/temp/search.cap-01.csv')
+    
+    data = read_csv()
+    
+    variable = tk.StringVar()
+    variable.set("Please Select a Network") # default value
+    
+    scan_button.destroy()    
+    OptionMenu_button = tk.OptionMenu(Assessment_window, variable, *data.keys())
+    select_button = tk.Button(Assessment_window, text='GO!',command= lambda: select_network(variable), font=("Helvetica 12 bold"))
+    OptionMenu_button.grid(row=3,column=0) 
+    select_button.grid(row=4,column=0) 
+    Assessment_window.update()
     
 def generate_report():
     print("This is the function to generate a report")
+    #writing HTML Content
+    heading = '<h1> Automated Report Tutorial by Abhinav Sharma</h1>'
+    subheading = '<h3> Sample Report for Placement </h3>'
+    # Using .now() from datetime library to add Time stamp
+    now = datetime.now()
+    current_time = now.strftime("%m/%d/%Y %H:%M:%S")
+    header = '<div class="top">' + heading + subheading +'</div>'
+    footer = '<div class="bottom"> <h3> This Report has been Generated on '+ current_time +'</h3> </div>'
+    content = '<div class="table"> ' + pivot.to_html() + ' </div> \n <div class="chart"> '+ image_tag +'</div>'
+    # Concating everything to a single string
+    html = header + content + footer    
     
 def menuAbout():
     about_window = tk.Toplevel()
@@ -424,11 +472,10 @@ if __name__ == '__main__':
     root.title("CYBV 498 - Wireless Security Assessment Tool")
     root.resizable(False, False)
     
-    #start_Assessment_button = tk.Button(text='Start Assessment',command=wifi_Assessment, font=("Helvetica 12 bold"))
-    start_Assessment_button = tk.Button(text='Test TransferFile',command=lambda: transfer_over_serial_thread('/root/temp/search.cap-01.csv'), font=("Helvetica 12 bold"))
+    start_Assessment_button = tk.Button(text='Start Assessment',command=wifi_Assessment, font=("Helvetica 12 bold"))
+    #start_Assessment_button = tk.Button(text='Read_File to tkinter',command=lambda: transfer_over_serial_thread('/root/temp/search.cap-01.csv'), font=("Helvetica 12 bold"))
     
     rogueAP_button = tk.Button(text='Rogue AP',command=rogueAP, font=("Helvetica 12 bold"))
-    wardriving_button = tk.Button(text='WarDriving',command=wardriving, font=("Helvetica 12 bold"))
     terminal_button=tk.Button(text='Serial Terminal',command=start_terminal, font=("Helvetica 12 bold"))
     
     image2 = Image.open("img/CYBV498.png")
@@ -436,11 +483,10 @@ if __name__ == '__main__':
     test2 = ImageTk.PhotoImage(image2)
     label2 = tk.Label(image=test2)
     
-    label2.grid(row=0,columnspan=2,padx=5, pady=10, ipadx=15, ipady=10)
-    start_Assessment_button.grid(row=1,columnspan=2,padx=5, pady=5, ipadx=5, ipady=5)
+    label2.grid(row=0,column=0,padx=5, pady=10, ipadx=15, ipady=10)
+    start_Assessment_button.grid(row=1,column=0,padx=5, pady=5, ipadx=5, ipady=5)
     rogueAP_button.grid(row=2,column=0,padx=5, pady=5, ipadx=5, ipady=5)
-    wardriving_button.grid(row=2,column=1,padx=5, pady=5, ipadx=5, ipady=5)
-    terminal_button.grid(row=3,columnspan=2,padx=5, pady=5, ipadx=5, ipady=5)
+    terminal_button.grid(row=3,column=0,padx=5, pady=5, ipadx=5, ipady=5)
     
     menuBar = tk.Menu(root)
     toolsMenu = tk.Menu(menuBar, tearoff=0)

a9cf8dc1368908ff4cc967745a46981b9f70c93c
Gabriel Haab <gabrielhaab@gmail.com>
2021-03-10 07:20:14 -0700

    Cleanup and function integration

--- 498.py
+++ 498.py
@@ -14,6 +14,7 @@ Version 1.5 March 2021 - Adjusted Assessment TKinter windows
 Version 1.6 March 2021 - Added comments
 Version 1.7 March 2021 - Capture Handshake + Network connect 
 Version 1.8 March 2021 - Minor Adjustments 
+Version 1.9 March 2021 - Cleanup and function integration
 '''
 
 #Third party imports
@@ -353,23 +354,30 @@ to this tool, which will then convert back to the original format.
 def transfer_over_serial(file_name):
     try:
         # clean buffer
-        # received_data = ser.read()              #read serial port
-        # time.sleep(1) #check for remaining byte
-        # received_data += ser.read(ser.inWaiting())  
+        received_data = ser.read()              #read serial port
+        time.sleep(1) #check for remaining byte
+        received_data += ser.read(ser.inWaiting())  
         # clean buffer
         
         b64data = send_command2("base64 " + file_name + " -w 0 | wc -c && echo") 
+        int(b64data)
         print("Lenght to be transferred: " + str(b64data), flush=True)
         #loading_bar(int(int(b64data)/12000),"Transferring file\n" + file_name)
         b64data = send_command2("base64 " + file_name + " -w 0 && echo") 
         print("Received: " + str(len(b64data)))
         
         print("Creating the file...")
+        
         b64data = b64data.encode()
         if '/' in file_name:
             file_name = file_name.split('/')[-1]
-        with open(file_name, "wb") as fh:
-            fh.write(base64.decodebytes(b64data))    
+        try:
+            with open(selected_network['ESSID'] + '/' + file_name, "wb") as fh:
+                fh.write(base64.decodebytes(b64data))    
+        except:
+            with open(file_name, "wb") as fh:
+                fh.write(base64.decodebytes(b64data))   
+                
         print("Done transferring the file: " + file_name)    
         return 1
     except Exception as err:
@@ -481,95 +489,156 @@ def wifi_Assessment():
 
 """
 ================================================================
-assessment_manager()
+get_password()
 This function is responsible for defining if the tool will perform
 an authenticated scan or not. 
 ================================================================
 """
-def assessment_manager(Assessment_window, message, next_button,back_button, Authentication):
-    if Authentication:
-        print("Authenticate Scanning")
-        back_button.destroy()
-        
+def get_password(Assessment_window, message, next_button,back_button):
+    
         message.config(text="Please type in the password for:\n\n ESSID: " + selected_network['ESSID'])
         password_input = tk.Entry(Assessment_window)
-        next_button.config(text="Next",command= lambda: analyze_traffic(Assessment_window, message, next_button,back_button,1))
+        var = tk.IntVar()
+        next_button.config(text="Next",command= lambda: var.set(0))
+        back_button.config(text="Skip",command= lambda: var.set(1))
         password_input.grid(row=1,columnspan=2,padx=5, pady=5, ipadx=5, ipady=5)
-
         Assessment_window.update()
-        
-    else:
-        print("Non-Authenticate Scanning") 
-        encryption_Manager()
+        next_button.wait_variable(var)
+        if var.get() == 0:
+            selected_network['PASS'] = password_input.get()
+        if var.get() == 1:
+            selected_network['PASS'] = False
+        password_input.destroy()
+        traffic_analyze()
 
-"""
-================================================================
-connect_to_wireless()
-This function is responsible for connecting the rapsberry pi to 
-the target network. This is used for an authenticated scan.
-================================================================
-"""       
-def connect_to_wireless(ESSID,PASS):
-    # This function will connect to the wireless via WPAClient():
-    print("Function to connect to the wireless network") 
-    # Creating/editing the WPA_Supplicant file.
-    send_command("echo 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev' > /etc/wpa_supplicant/wpa_supplicant.conf")
-    send_command("echo 'update_config=1' >> /etc/wpa_supplicant/wpa_supplicant.conf")
-    send_command("echo 'country=US' >> /etc/wpa_supplicant/wpa_supplicant.conf")
-    send_command("echo ' ' >> /etc/wpa_supplicant/wpa_supplicant.conf")
-    send_command("echo 'network={' >> /etc/wpa_supplicant/wpa_supplicant.conf")
-    send_command("echo '        ssid=\"" + ESSID + "\"' >> /etc/wpa_supplicant/wpa_supplicant.conf")
-    send_command("echo '        psk=\"" + PASS + "\"' >> /etc/wpa_supplicant/wpa_supplicant.conf")
-    send_command("echo '        scan_ssid=1' >> /etc/wpa_supplicant/wpa_supplicant.conf")
-    send_command("echo '}' >> /etc/wpa_supplicant/wpa_supplicant.conf")
-    print("Done Creating file")
-    time.sleep(2)
-    print("Attempting to connect to the wireless")
-    send_command("sudo wpa_cli -i wlan0 reconfigure")
-    
-    received_data = ser.read()              #read serial port
-    time.sleep(1) #check for remaining byte
-    received_data += ser.read(ser.inWaiting())     
-    
-    print("Waiting for Connection...")
-    time.sleep(15) 
-    
-    send_command("iwconfig wlan0 | head -1")
-    
-    connection = ''
-    received_data = ser.read()              #read serial port
-    time.sleep(1)
-    data_left = ser.inWaiting()             #check for remaining byte
-    received_data += ser.read(data_left)
-    rcvd = received_data.decode('ascii')
-    #print(rcvd)
-    
-    for line in rcvd.split('\r\n'):
-        if 'raspberrypi' not in line and 'iwconfig' not in line:
-            connection = line
-    
-    print("Connection value: " + connection)
-    
-    if 'ESSID:off/' in connection: 
-        print("Failed to connect")
-        return 0
-    else:
-        print("Done Connecting. Please Check the interface")
-        return 1        
-        
 """
 ================================================================
 Function to perform an active scan in the network. 
 ================================================================
 """    
-def analyze_traffic():
-    # This fucntion will make a dump of the network likely through tshark.
-    # This function will perform an active scan.
-    # This function will perform an nmap scan.
-    # This function will also generate sobre analyses through Panda.
-    print("Function to analyze the network")
-    connect_to_wireless(selected_network['ESSID'],password_input.get())
+def traffic_analyze():
+    if selected_network['PASS'] == False:
+        return 0
+    
+    print("start of traffic_analyze()")
 
+    # Decrypt File    
+    print("Decrypt Network Traffic")
+    # https://www.aircrack-ng.org/doku.php?id=airdecap-ng
+    send_command("airdecap-ng -e '" + selected_network['ESSID'] + "' -p " + selected_network['PASS'] + " /root/temp/" + selected_network['ESSID'] + "-01.cap")
+    time.sleep (10)
+    
+    print("PCAP to .CSV")
+    send_command("tshark -r " + "/root/temp/" + selected_network['ESSID'] + "-01-dec.cap" + " -T fields -e frame.number -e frame.time -e eth.src -e eth.dst -e ip.src -e _ws.col.SrcPort -e _ws.col.DstPort -e ip.dst -e _ws.col.Protocol -E header=y -E separator=, -E quote=d -E occurrence=f > tshark_file.csv")
+    time.sleep(10)
+    # It outputs a new file ending with -dec.cap which is the decrypted/stripped version of the input file.
+    
+    print("Transferring files")
+    x=0
+    while x == 0:
+        x = transfer_over_serial('/root/temp/tshark_file.csv')
+        time.sleep(5)    
+    
+    # ======================= Analyse File ===============
+    try:
+        df_dump = pd.read_csv(selected_network['ESSID'] + "/tshark_file.csv")
+        
+        possible_protocols = ['DHCP','ARP', 'DNS', 'TCP','UDP']
+        
+        df_dump = df_dump.rename(columns={'frame.number': 'Frame', 'frame.time': 'Time', 'eth.src': 'Source_Mac', 'eth.dst': 'Destination_Mac', 'ip.src': 'IP_Source', 'ip.dst':'IP_Destination', '_ws.col.Protocol':'Protocol', '_ws.col.SrcPort':'Source_Port', '_ws.col.DstPort':'Destination_Port'})
+       
+        df_dump = df_dump[df_dump['Protocol'].isin(possible_protocols)]
+        print(df_dump.head().to_string())
+        print("\n\n====================\n\n")
+        
+        # ======================= Protocol count =======================
+        
+        df_dump_prot = df_dump.groupby('Protocol').Frame.count()
+        print(df_dump_prot.to_string())
+        
+        ax = df_dump_prot.plot(kind='bar', title='protocol by count', rot=15)
+        ax.set_xlabel("Protocol")
+        ax.set_ylabel("count")
+    
+        fig = ax.get_figure()
+        fig.savefig(selected_network['ESSID'] + '/Protocol_Graph.jpeg')    
+        
+        # ======================= Source IP packet count =======================
+        
+        df_dump_srcip_count = df_dump.groupby('IP_Source').Frame.count()
+        ax = df_dump_srcip_count.plot(kind='bar', title='Source IP by count', rot=15)
+        ax.set_xlabel("IP")
+        ax.set_ylabel("Count")
+    
+        fig = ax.get_figure()
+        fig.savefig(selected_network['ESSID'] + '/Source_IPCount_Graph.jpeg')    
+        
+        # ======================= Destination IP packet count =======================
+        
+        df_dump_destip_count = df_dump.groupby('IP_Destination').Frame.count()
+        ax = df_dump_destip_count.plot(kind='bar', title='Destination IP by count', rot=15)
+        ax.set_xlabel("IP")
+        ax.set_ylabel("Count")
+    
+        fig = ax.get_figure()
+        fig.savefig(selected_network['ESSID'] + '/Dest_IPCount_Graph.jpeg')
+    except Exception as err:
+        print("unable to decrypt traffic: " + str(err))
+    print("End of traffic_analyze()")
+
+"""
+================================================================
+dump_traffic. 
+This fucntion will make a dump of the network likely through tshark.
+This function will perform an active scan.
+This function will perform an nmap scan.
+This function will also generate sobre analyses through Panda.
+================================================================
+"""    
+def dump_traffic():
+    print("starting dump_traffic()")
+    
+    remove_temp()
+    dump_time = 120 # time in seconds to analyze the traffic
+    
+    print("Enabling monitor mode")
+    monitor_mode("enable", "wlan1", selected_network['Channel'])
+    
+    print("Starting Dump")
+    send_command("tmux new -s 'capture_handshake' -d")
+    send_command("tmux new-window -t 'capture_handshake' -n:airodump-ng 'timeout " +  str(dump_time) + "s airodump-ng -c "  + selected_network['Channel'] + " --bssid " + selected_network['BSSID'] + " -w /root/temp/" + selected_network['ESSID'] + " wlan1mon'")
+    
+    
+    # Capture the handshake
+    time.sleep(5)
+    send_command("aireplay-ng -0 10 -a " + selected_network['BSSID'] + " wlan1mon")
+    time.sleep(10)
+    send_command("aireplay-ng -0 10 -a " + selected_network['BSSID'] + " wlan1mon")
+    time.sleep(10)
+    time.sleep(5)
+    
+    loading_bar(dump_time-30,'Creating a network dump')
+    
+    send_command("tmux kill-session -t 'capture_handshake'")
+    
+    print("Disable monitor mode:")
+    monitor_mode("disable", "wlan1", selected_network['Channel'])
+    
+    print("End of dump_traffic()")    
+
+"""
+================================================================
+handshake_Hash()
+This function is responsible for generating a hash of the handshake
+This can later be used for offline password cracking.
+================================================================
+"""  
+def handshake_Hash():
+    print("Start of handshake_Hash()")
+    selected_network['WPAHash'] = 'something something'
+    
+    print("End of handshake_Hash()")
+    
 """
 ================================================================
 encryption_Manager()
@@ -577,20 +646,42 @@ This function is responsible for managing the different attacks
 depending on the encryption method being used.
 ================================================================
 """    
-def encryption_Manager(encryption):
-    if encryption == "WEP":
-        print ("The Encryption is WEP") 
-        #capture_IV()
+def encryption_Manager(Assessment_window, message, next_button,back_button):
+    
+    #Create the folder to hold all the analysis
+    try:
+        os.mkdir(selected_network['ESSID']) 
+    except:
+        # Delete all contents of the directory
+        for file in os.listdir(selected_network['ESSID']):
+            os.remove(selected_network['ESSID'] + '/' + file)
+        # Delete the empty folder
+        os.rmdir(selected_network['ESSID'])
+        # Create the folder. 
+        os.mkdir(selected_network['ESSID']) 
         
-    if encryption == ("WPA" or "WPA2"):
-        print ("The Encryption is WEP") 
-        capture_Handshake()
-        airgraph("/root/temp/" + selected_network[BSSID] + ".csv")
+    if "WEP" in selected_network['Privacy']:
+        print("The Encryption is WEP") 
+        dump_traffic()
         
-    if encryption == "WPA3":
-        print ("The Encryption is WPA3")
-        capture_handshake()
-        airgraph("/root/temp/" + selected_network[BSSID] + ".csv")   
+    elif "WPA3" in selected_network['Privacy']:
+        print ("The Encryption is WPA3")   
+        dump_traffic()
+        compare_channels(selected_network['Channel'])
+        airgraph("/root/temp/" + selected_network['ESSID'] + ".csv")
+        generate_report(Assessment_window, message, next_button,back_button)
+        
+    elif "WPA" in selected_network['Privacy']:
+        print ("The Encryption is WPA or WPA2") 
+        dump_traffic()
+        handshake_Hash()
+        get_password(Assessment_window, message, next_button,back_button)
+        compare_channels(selected_network['Channel'])
+        airgraph("/root/temp/" + selected_network['ESSID'] + "-01.csv")
+        generate_report(Assessment_window, message, next_button,back_button)
+    
+    else:
+        print("I don't know this encryption")
         
 """
 ================================================================
@@ -611,48 +702,7 @@ def monitor_mode(mode,card,channel):
         #send_command("iw dev mon0 interface del")
         send_command("airmon-ng stop " + card + "mon")
         time.sleep(3) # wait 3 seconds just in case.
-
-"""
-================================================================
-capture_handshake()
-This function performs the attack to capture the 4way handshake.
-================================================================
-"""    
-def capture_handshake(selected_network):
-    '''
-    selected_network = {}
-    selected_network['channel'] = '3'
-    selected_network['BSSID'] = '00:18:E7:E9:32:E2'
-    selected_network['ESSID'] = 'CYBV_498_Test'
-    
-    print("capture_handshake")
-    selected_network
-    '''
-    remove_temp() 
-    print("Enabling monitor mode")
-    monitor_mode("enable", "wlan1", selected_network['channel'])
-    
-    print("starting tmux")
-    send_command("tmux new -s 'capture_handshake' -d")
-    send_command("tmux new-window -t 'capture_handshake' -n:airodump-ng 'timeout 35s airodump-ng -c "  + selected_network['channel'] + " --bssid " + selected_network['BSSID'] + " -w /root/temp/" + selected_network['ESSID'] + " wlan1mon'")
-    
-    print("starting aireplay")
-    
-    # Deauthentication
-    time.sleep(5)
-    send_command("aireplay-ng -0 10 -a " + selected_network['BSSID'] + " wlan1mon")
-    time.sleep(10)
-    send_command("aireplay-ng -0 10 -a " + selected_network['BSSID'] + " wlan1mon")
-    time.sleep(10)
-    time.sleep(5)
-    
-    print("kill tmux session")
-    send_command("tmux kill-session -t 'capture_handshake'")
-                 
-    print("Disable monitor mode")
-    monitor_mode("disable","wlan1","")
-    print("done") 
-    
+   
 """
 ================================================================
 airgraph()
@@ -749,6 +799,7 @@ other functions.
 """
 def select_network(Assessment_window, message, next_button,back_button,OptionMenu_button,network_name): 
     global selected_network
+    
     Assessment_window.update()
     selected_network = {}
     ESSID = network_name.get()
@@ -761,8 +812,8 @@ def select_network(Assessment_window, message, next_button,back_button,OptionMen
         OptionMenu_button.destroy()
         
         message.config(text="You Selected:\n\n ESSID: " + selected_network['ESSID'] + " \nEncryption: " + selected_network['Privacy'] + " \nBSSID: " + selected_network['BSSID'] + " \nChannel: " + selected_network['Channel'])
-        next_button.config(text="Authenticated Scan",command= lambda: assessment_manager(Assessment_window, message, next_button,back_button,1))
-        back_button.config(text="Non-Authenticated Scan",command= lambda: assessment_manager(Assessment_window, message, next_button,back_button,0))                   
+        next_button.config(text="Next",command= lambda: encryption_Manager(Assessment_window, message, next_button,back_button))
+        back_button.config(text="Quit",command=Assessment_window.destroy)                   
     else:
         messagebox.showinfo("ERROR", "PLEASE SELECT A NETWORK\nFROM THE DROPDOWN MENU")
         scanNetwork(Assessment_window, message, next_button,back_button)
@@ -777,8 +828,7 @@ automatically replaces the tags.
 ================================================================
 """    
 def generate_report(Assessment_window, message, next_button,back_button):
-    print("This is the function to generate a report")
-    compare_channels(selected_network['Channel'])
+    print("This is the function to generate a report")  
     
     # Integrate function here
     
@@ -791,8 +841,6 @@ def generate_report(Assessment_window, message, next_button,back_button):
     back_button.config(text="Open Report", command= lambda: os.startfile("Report.pdf"))
     next_button.grid(row=2,columnspan=2,padx=5, pady=5, ipadx=5, ipady=5)
     Assessment_window.update()
-    
-    monitor_mode("disable","wlan1")
 
 """
 ================================================================
@@ -804,9 +852,9 @@ channels.
 """    
 def compare_channels(target):
     all_channels = [1,2,3,4,5,6,7,8,9,10,11,12]
-    df=pd.DataFrame.from_dict(data,orient='index')
+    df_channels=pd.DataFrame.from_dict(data,orient='index')
     
-    hue = df[2].value_counts()
+    hue = df_channels[2].value_counts()
     target_num = 0 
     for index, value in hue.items():
         all_channels.remove(int(index))
@@ -825,7 +873,7 @@ def compare_channels(target):
     ax.plot(int(target),int(target_num), marker='o',markersize=10, color="red")
     
     fig = ax.get_figure()
-    fig.savefig('Compare_Channels.jpeg')
+    fig.savefig(selected_network['ESSID'] + '/Compare_Channels.jpeg')
     print("done")
 
 """
@@ -867,13 +915,12 @@ if __name__ == '__main__':
     root.resizable(False, False)
     
     start_Assessment_button = tk.Button(text='Start Assessment',command=wifi_Assessment, font=("Helvetica 12 bold"))
-    #start_Assessment_button = tk.Button(text='Download File',command=lambda: transfer_over_serial('csvFile.csv'), font=("Helvetica 12 bold"))
+    #start_Assessment_button = tk.Button(text='Download File',command=lambda: transfer_over_serial('Client_To_AP.png'), font=("Helvetica 12 bold"))
     
     rogueAP_button = tk.Button(text='Rogue AP',command=rogueAP, font=("Helvetica 12 bold"))
     terminal_button=tk.Button(text='Serial Terminal',command=start_terminal, font=("Helvetica 12 bold"))
     power_off_button = tk.Button(text='Power Off',command=lambda: send_command1('shutdown -h now'), font=("Helvetica 12 bold"))
-    
-    
+
     image2 = Image.open("img/CYBV498.png")
     image2 = image2.resize((150, 130), Image.ANTIALIAS)    
     test2 = ImageTk.PhotoImage(image2)
